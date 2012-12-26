8155144abf616af5e7c0964df2842f7844e2de55
Xabier Laiseca <xabierlaiseca@gmail.com>
2012-12-26 23:40:13 +0000

    Added the = after the main method

--- scala/helloworld/src/main/scala/helloworld.scala
+++ scala/helloworld/src/main/scala/helloworld.scala
@@ -1,7 +1,7 @@
 package scala.examples.helloworld
 
 object HelloWorld {
-    def main(args: Array[String]) {
+    def main(args: Array[String]) = {
         println("Hello Scala world!!")
     }
 }
