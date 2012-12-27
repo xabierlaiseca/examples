e70284bb7631ccc6450c14f6a5cadad0f725ef2a
Xabier Laiseca <xabierlaiseca@gmail.com>
2012-12-27 12:11:33 +0000

    Created the parameters example

---
+++ scala/methods/src/main/scala/parameters.scala
@@ -0,0 +1,17 @@
+package scala.examples.methods
+
+object ParametersExample {
+    def main(args: Array[String]) = {
+        val paramExample = new ParametersExample
+        paramExample.printArray(Array("Parameters", "Example"))
+        paramExample.printElements("Parameters", "Example")
+    }
+}
+
+class ParametersExample {
+    def printArray(array: Array[String]): Unit = 
+        array.foreach(println(_))
+
+    def printElements(elems: String*): Unit = 
+        elems.foreach(println(_))
+}
